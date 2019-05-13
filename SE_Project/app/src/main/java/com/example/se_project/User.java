6add9b083bcbe77cd5e34558481c1774c15d7cb2
陈达寅 <11612512@mail.sustc.edu.cn>
2019-05-13 11:49:38 +0800

    Add the property of portait, modify some UI

--- SE_Project/app/src/main/java/com/example/se_project/User.java
+++ SE_Project/app/src/main/java/com/example/se_project/User.java
@@ -4,12 +4,14 @@ public class User {
     private String name;
     private double gpa;
     private String id;
+    private int potraitnum; //头像ID
 
     public User(){}
 
     public User(String name,double gpa){
         this.gpa=gpa;
         this.name=name;
+        this.setPotraitnum();
     }
 
     public String getName(){
@@ -33,5 +35,18 @@ public class User {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    private void setPotraitnum(){
+        if (this.gpa>=3.7){
+            potraitnum = 3;
+        }else if (this.gpa>=3.2){
+            potraitnum = 2;
+        }else if (this.gpa>=2.8){
+            potraitnum = 1;
+        }else{
+            potraitnum = 0;
+        }
+
     }
 }
