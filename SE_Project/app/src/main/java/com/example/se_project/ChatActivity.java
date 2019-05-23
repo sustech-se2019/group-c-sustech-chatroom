f786079222ba665bf5530bcd835453c096da7242
陈达寅 <11612512@mail.sustc.edu.cn>
2019-05-23 11:23:15 +0800

    Fix the bug of Album, implement Choose image

--- SE_Project/app/src/main/java/com/example/se_project/ChatActivity.java
+++ SE_Project/app/src/main/java/com/example/se_project/ChatActivity.java
@@ -96,13 +96,11 @@ public class ChatActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(ChatActivity.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(ChatActivity.this, new
-                            String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                } else {
-//打开系统相册
-                    openAlbum();
+                switch (v.getId()){
+                    case R.id.Image:
+                        Intent intent = new Intent();
+                        intent.setClass(ChatActivity.this, UploadActivity.class);
+                        ChatActivity.this.startActivity(intent);
                 }
             }
         });
@@ -337,117 +335,6 @@ public class ChatActivity extends AppCompatActivity {
 
     public void setAdapter(MsgAdapter adapter) {
         this.adapter = adapter;
-    }public String getSDPath(){
-        File sdDir = null;
-        boolean sdCardExist = Environment.getExternalStorageState()
-                .equals(android.os.Environment.MEDIA_MOUNTED);   //判断sd卡是否存在
-        if   (sdCardExist)
-        {
-            sdDir = Environment.getExternalStorageDirectory();//获取跟目录
-        }
-        return sdDir.toString();
-
-    }
-
-
-    private String getPhotoFileName() {
-        Date date = new Date(System.currentTimeMillis());
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-                "'IMG'_yyyyMMdd_HHmmss");
-        return dateFormat.format(date)  +".jpg";
-    }
-
-    //打开系统相册
-    private void openAlbum() {
-        Intent intent = new Intent("android.intent.action.GET_CONTENT");
-        intent.setType("image/*");
-        startActivityForResult(intent, REQUEST_SYSTEM_PIC);//打开系统相册
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openAlbum();
-                } else {
-                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //相册照片
-        if (requestCode == REQUEST_SYSTEM_PIC && resultCode == RESULT_OK && null != data) {
-            if (Build.VERSION.SDK_INT >= 19) {
-                handleImageOnKitkat(data);
-            } else {
-                handleImageBeforeKitkat(data);
-            }
-        }
-        //相机照片
-        if (requestCode == CAMERA_RESULT) {
-            Bitmap bitmap = BitmapFactory.decodeFile(mPhotoPath, null);
-            mImageView.setImageBitmap(bitmap);
-        }
-    }
-    @TargetApi(19)
-    private void handleImageOnKitkat(Intent data) {
-        String imagePath = null;
-        Uri uri = data.getData();
-        if (DocumentsContract.isDocumentUri(this, uri)) {
-            //如果是document类型的uri，则通过document id处理
-            String docId = DocumentsContract.getDocumentId(uri);
-            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
-                String id = docId.split(":")[1];
-                String selection = MediaStore.Images.Media._ID + "=" + id;
-                imagePath = getImagePath( MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
-            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
-                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content:" +
-                        "//downloads/public_downloads"), Long.valueOf(docId));
-                imagePath = getImagePath(contentUri, null);
-            }
-        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
-            //如果是content类型的uri，则使用普通方式处理
-            imagePath = getImagePath(uri, null);
-        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            //如果是File类型的uri，直接获取图片路径即可
-            imagePath = uri.getPath();
-        }
-        //根据图片路径显示图片
-        displayImage(imagePath);
-
-    }
-
-    private void handleImageBeforeKitkat(Intent data) {
-        Uri uri = data.getData();
-        String imagePath = getImagePath(uri, null);
-        displayImage(imagePath);
-
-    }
-    private String getImagePath(Uri uri, String selection) {
-        String path = null;
-        //通过uri和selection来获取真实的图片路径
-        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-            }
-            cursor.close();
-        }
-        return path;
-    }
-    private void displayImage(String imagePath) {
-        if (imagePath != null) {
-            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-            mImageView.setImageBitmap(bitmap);
-        } else {
-            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
-        }
     }
 
 
