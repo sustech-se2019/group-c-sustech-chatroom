589a5029f2139127cd7ec527bc5aac0936463976
陈达寅 <11612512@mail.sustc.edu.cn>
2019-05-24 20:49:58 +0800

    Normal commit

--- SE_Project/app/src/main/java/com/example/se_project/UserAddAdapter.java
+++ SE_Project/app/src/main/java/com/example/se_project/UserAddAdapter.java
@@ -2,6 +2,8 @@ package com.example.se_project;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
